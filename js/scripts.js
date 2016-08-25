92baefa76a51113e12cc1dc06fd8f332a841cdc1
Lisa MacCarrigan and Sam Peppard <lisa@>
2016-08-24 23:15:03 -0400

    add logic to properly conduct pig rules

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,7 @@ var Player = function(turn) {
   this.scoreTotal = 0;
   this.currentScore = 0;
   this.turn = turn;
-}
+};
 
 Player.prototype.takeTurn = function() {
   if (this.turn === true) {
@@ -13,24 +13,16 @@ Player.prototype.takeTurn = function() {
   else {
     this.turn = true;
   }
-}
+};
 
 Player.prototype.roll = function() {
   var rand = Math.floor(Math.random() * 6) + 1;
   this.currentScore = rand;
-}
+};
 
-Player.prototype.rollAgain = function() {
-  var rand2 = Math.floor(Math.random() * 6) + 1;
-  this.currentScore = rand2;
-  if (this.currentScore >= 2) {
-    this.scoreTotal += this.currentScore;
-  }
-}
-
-Player.prototype.hold = function() {
-  this.scoreTotal += this.currentScore;
-}
+Player.prototype.scorePoints = function(rollPoints) {
+  this.scoreTotal += rollPoints;
+};
 
 var player1 = new Player(true);
 var player2 = new Player(false);
@@ -57,68 +49,60 @@ $(document).ready(function() {
         $("button").attr("readonly", true);
         alert("Looks like we have a winner!!!!!  ;)");
       }
-      else if ($(this).hasClass("roll-again")) {
-        player1.rollAgain();
-        player1.takeTurn();
-        player2.takeTurn();
-        $("#rolled-number").text(player1.currentScore);
-        $(".p1-total-score").text(player1.scoreTotal);
-        $(this).removeClass("roll-again").text("Roll");
-        $("#player-msg").text(playerTwoName + ", GO!");
-      }
       else {
         player1.roll();
-        $("#rolled-number").text(player1.currentScore);
-        $(this).addClass("roll-again").text("Roll Again?");
+        if (player1.currentScore === 1) {
+          player1.takeTurn();
+          player2.takeTurn();
+          $("#rolled-number").text(player1.currentScore);
+          $(this).addClass("roll-again").text("Roll");
+          $("#player-msg").text(playerTwoName + ", GO!");
+        }
+        else {
+          $("#rolled-number").text(player1.currentScore);
+          $(this).addClass("roll-again").text("Roll Again?");
+        }
       }
     }
     else {
       if (player2.scoreTotal >= 100 || player1.scoreTotal >= 100) {
         $("button").attr("readonly", true);
       }
-      else if ($(this).hasClass("roll-again")) {
-        player2.rollAgain();
-        player1.takeTurn();
-        player2.takeTurn();
-        $("#rolled-number").text(player2.currentScore);
-        $(".p2-total-score").text(player2.scoreTotal);
-        $(this).removeClass("roll-again").text("Roll");
-        $("#player-msg").text(playerOneName + ", GO!");
-      }
       else {
         player2.roll();
-        $("#rolled-number").text(player2.currentScore);
-        $(this).addClass("roll-again").text("Roll Again?");
+        if (player2.currentScore === 1) {
+          player1.takeTurn();
+          player2.takeTurn();
+          $("#rolled-number").text(player2.currentScore);
+          $(this).removeClass("roll-again").text("Roll");
+          $("#player-msg").text(playerOneName + ", GO!");
+        }
+        else {
+          $(this).addClass("roll-again").text("Roll Again?");
+          $("#rolled-number").text(player2.currentScore);
+        }
       }
     }
-
-    $("#hold").click(function() {
-      if (player1.turn === true) {
-        player1.takeTurn();
-        player2.takeTurn();
-        player1.hold();
-        $("#rolled-number").text(player1.currentScore);
-        $(".p1-total-score").text(player1.scoreTotal);
-        $("#roll").removeClass("roll-again").text("Roll");
-        $("#player-msg").text(playerTwoName + ", GO!");
-        //$(this).attr("readonly", true);
-      }
-      else {
-        player1.takeTurn();
-        player2.takeTurn();
-        player2.hold();
-        $("#rolled-number").text(player2.currentScore);
-        $(".p2-total-score").text(player2.scoreTotal);
-        $("#roll").removeClass("roll-again").text("Roll");
-        $("#player-msg").text(playerOneName + ", GO!");
-        //$(this).attr("readonly", true);
-      }
-    });
-
-    console.log(player1.turn);
-    console.log(player2.turn);
-    console.log("player 1 score: " + player1.scoreTotal);
-    console.log("player 2 score: " + player2.scoreTotal);
   }); // end click
+
+  $("#hold").click(function() {
+    var currentRole = parseInt($("#rolled-number").text());
+    if (player1.turn === true) {
+      player1.takeTurn();
+      player2.takeTurn();
+      player1.scorePoints(currentRole);
+      $(".p1-total-score").text(player1.scoreTotal);
+      $("#roll").removeClass("roll-again").text("Roll");
+      $("#player-msg").text(playerTwoName + ", your turn!");
+    }
+    else {
+      player1.takeTurn();
+      player2.takeTurn();
+      player2.scorePoints(currentRole);
+      $(".p2-total-score").text(player2.scoreTotal);
+      $("#roll").removeClass("roll-again").text("Roll");
+      $("#player-msg").text(playerOneName + ", your turn!");
+    }
+  });
 
 }); // end ready
